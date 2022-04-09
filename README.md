4a0371b36d45932087bf908b6ff690f09260eb2b
小草 <52877059+xiaocao666tzh@users.noreply.github.com>
2022-04-09 15:02:37 +0800

    Add code

---
+++ README.md
@@ -0,0 +1,22 @@
+# 机场自动签到
+做这个项目主要是看到最近冒出来许多机场注册就能白嫖，签到就送流量。然后又顺便拿这个项目学习一下Github Actions。
+## 功能
+废话，肯定就是机场自动签到啊。
+## 部署
+1. Fork此仓库
+2. 到`Settings`→`Secrets`→`Actions` 添加以下参数：
+
+| 参数  | 是否必须  | 内容  | 示例  |
+| ------------ | ------------ | ------------ | ------------ |
+| EMAIL  | 是  | 注册机场所用邮箱  | example@example.com  |
+| PASSWORD  | 是  | 注册机场所用密码  | password  |
+| BASE_URL  | 是  | 机场地址  | https://example.com  |
+| SCKEY  | 是  | Sever酱秘钥  | SCTxxxxxxxxxxxxxx  |
+
+3. 转到`Actions`创建一个workflow，运行一次，以后每天项目都会自动运行。最后，可以到Run sign查看签到情况，同时也会通过Sever酱发送出去。
+## 参考
+1. https://github.com/zhjc1124/ssr_autocheckin 用了他的机场签到代码。
+2. https://github.com/sirodeneko/genshin-sign 参考其Actions的yml。
+（两个仓库均无许可证，让我有点蒙）
+## 赞助我
+[![](https://raw.githubusercontent.com/xiaocao666tzh/imghosting/main/img/%E4%B8%87%E8%83%BD%E6%94%B6%E6%AC%BE%E7%A0%81-%E8%8D%89%E3%81%AE%E5%8D%9A%E5%AE%A2.png)](https://raw.githubusercontent.com/xiaocao666tzh/imghosting/main/img/%E4%B8%87%E8%83%BD%E6%94%B6%E6%AC%BE%E7%A0%81-%E8%8D%89%E3%81%AE%E5%8D%9A%E5%AE%A2.png)
---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import os
+requests.packages.urllib3.disable_warnings()
+SCKEY = os.environ['SCKEY']
+
+def checkin(email= os.environ['EMAIL'] ,password= os.environ['PASSWORD'] ,base_url= os.environ['BASE_URL']):
+    email = email.split('@')
+    email = email[0] + '%40' + email[1]
+
+    session = requests.session()
+    
+    session.get(base_url, verify=False)
+
+    login_url = base_url + '/auth/login'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    }
+
+    post_data = 'email=' + email + '&passwd=' + password + '&code='
+    post_data = post_data.encode()
+    response = session.post(login_url, post_data, headers=headers, verify=False)
+    
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        'Referer': base_url + '/user'
+    }
+
+    response = session.post(base_url + '/user/checkin', headers=headers, verify=False)
+    response = json.loads(response.text)
+    print(response['msg'])
+    return response['msg']
+
+r = requests.get(url = 'https://sctapi.ftqq.com/'+ SCKEY +  '.send?title=机场签到&desp=' + checkin()
+)
